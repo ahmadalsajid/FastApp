1f0c84c346c7bc18262e9d1e359f69e5b25f0684
ahmadalsajid <ahmadalsajid@gmail.com>
2024-07-31 03:07:56 +0600

    git tagging not working

--- tagging/dev_git_tagging.sh
+++ tagging/dev_git_tagging.sh
@@ -59,9 +59,9 @@ NEEDS_TAG=$(git describe --contains $GIT_COMMIT 2>/dev/null)
 # only tag if no tag already
 if [ -z "$NEEDS_TAG" ]; then
   echo "Tagged with $NEW_TAG"
-  git tag $NEW_TAG
-  git push --tags
-  git push
+#  git tag $NEW_TAG
+#  git push --tags
+#  git push
 else
   echo "Already a tag on this commit"
 fi
